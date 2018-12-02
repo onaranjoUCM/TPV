f13e24e29c78f7f0ae2928c8205234c6b77da0e1
AlejandroPla <37513637+AlejandroPla@users.noreply.github.com>
2018-12-02 16:24:25 +0100

    menu expandido/pendiente de revision

--- HolaSDL/Menu.cpp
+++ HolaSDL/Menu.cpp
@@ -10,7 +10,7 @@ Menu::Menu() {
 		MENU_WIDTH, MENU_HEIGHT, SDL_WINDOW_SHOWN);
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	if (window == nullptr || renderer == nullptr) throw "Error loading the SDL window or renderer";
-
+	//TITLE
 	titleRect.x = 100;
 	titleRect.y = 100;
 	titleRect.w = 600;
@@ -18,23 +18,39 @@ Menu::Menu() {
 	titleText = new Texture(renderer);
 	titleText->load("..\\images\\title.png", 1, 1);
 
+	// play button
 	playButtonRect.x = 350;
-	playButtonRect.y = 400;
+	playButtonRect.y = 425; // de 400 a 450
 	playButtonRect.w = 100;
 	playButtonRect.h = 50;
 	playButtonText = new Texture(renderer);
 	playButtonText->load("..\\images\\bricks.png", 2, 3);
 
+	// load button
 	loadButtonRect.x = 350;
-	loadButtonRect.y = 500;
+	loadButtonRect.y = 525; // de 500 a 550
 	loadButtonRect.w = 100;
 	loadButtonRect.h = 50;
 	loadButtonText = new Texture(renderer);
 	loadButtonText->load("..\\images\\bricks.png", 2, 3);
+
+	// end button
+	endButtonRect.x = 350;
+	endButtonRect.y = 325; // de 300 a 350
+	endButtonRect.w = 100;
+	endButtonRect.h = 50;
+	endButtonText = new Texture(renderer);
+	endButtonText->load("..\\images\\bricks.png", 2, 3);
 }
 
 Menu::~Menu() {
-
+	// destruye los punteros
+	window = nullptr;
+	renderer = nullptr;
+	playButtonText = nullptr;
+	endButtonText = nullptr;
+	loadButtonText = nullptr;
+	titleText = nullptr;
 }
 
 // Ejecución del juego
@@ -46,17 +62,16 @@ void Menu::run() {
 	}
 }
 
-// Métodos que ocurren en caada frame
-void Menu::update() {
+// Métodos que ocurren en cada frame
+void Menu::update() {} //NOTA = PREGUNTAR SI BORRAR METODO
 
-}
-
-// Muestra todos los objetos en pantaalla
+// Muestra todos los objetos en pantalla
 void Menu::render() {
 	SDL_RenderClear(renderer);
 	titleText->render(titleRect, SDL_FLIP_NONE);
-	playButtonText->renderFrame(playButtonRect, 0, 2);
-	loadButtonText->renderFrame(loadButtonRect, 0, 0);
+	playButtonText->renderFrame(playButtonRect, 0, 0); //ROJO = JUGAR
+	loadButtonText->renderFrame(loadButtonRect, 0, 1); //AZUL = CARGAR
+	endButtonText->renderFrame(endButtonRect, 0, 2); //VERDE = SALIR
 	SDL_RenderPresent(renderer);
 }
 
@@ -68,11 +83,27 @@ void Menu::handleEvents() {
 
 		if (event.type == SDL_KEYDOWN) {
 			Game game = Game();
+			string loadCode;
+			switch (event.key.keysym.sym)
+			{
+			case SDLK_p:
+				game.run();
+				break;
+			case SDLK_l:
+				cin >> loadCode;
+				cout << "Introducido el código: " << loadCode;
+				break;
+			case SDLK_e:
+				exit = true;
+				cout << "Se ha terminado el juego";
+				break;
+			}
+			/*Game game = Game();
 			game.run();
-			exit = true;
+			exit = true;*/
 		}
-
-		if (event.button.button == SDL_BUTTON_LEFT)
+		// NOTA: revisar si sirve el loadGame
+		/*if (event.button.button == SDL_BUTTON_LEFT)
 		{
 			int x = event.button.x;
 			int y = event.button.y;
@@ -91,8 +122,6 @@ void Menu::handleEvents() {
 				cout << "Introducido el código: " << loadCode;
 				// Cargar nivel delde el archivo loadCode
 			}
-		}
-
+		}*/
 	}
 }
-
